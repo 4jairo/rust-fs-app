0399427f735b0686430399b9f82917741930911b
jairo <jairotecles@gmail.com>
2024-02-09 18:22:22 +0100

    little improvement in search time

--- src-tauri/src/fs_app/cacher/discover.rs
+++ src-tauri/src/fs_app/cacher/discover.rs
@@ -2,6 +2,7 @@ use lazy_static::lazy_static;
 use rayon::prelude::*;
 use std::{path::PathBuf, sync::Mutex};
 use std::fs::{self, ReadDir};
+use crate::fs_app::dir_content::DirContent;
 use crate::fs_app::HashMapTreeType;
 use crate::fs_app::error_format::stringify;
 use crate::fs_app::get_os_disks::{DISKS, DiskStatus};
@@ -39,41 +40,64 @@ fn walk_from_dir(files: ReadDir) {
 
 pub struct DirTreeSearcher;
 impl DirTreeSearcher {
-    pub fn not_case_sensitive_exact_name(lower_value: &str) -> Vec<(String, Vec<PathBuf>)> {
+    pub fn not_case_sensitive_exact_name(lower_value: &str, search_path: &str) -> Vec<DirContent> {
         let dir_tree_cache = &DIR_TREE_CACHE.lock().unwrap();
         dir_tree_cache
             .par_iter()
             .filter(|(key, _)| key.to_lowercase() == lower_value)
-            .map(|(key, paths)| (key.clone(), paths.to_owned()))
-            .collect::<Vec<(String, Vec<PathBuf>)>>()
+            .flat_map(|(name, paths)| to_dir_content(name, paths, search_path))
+            .collect()
     }
 
-    pub fn case_sensitive_exact_name(value: &str) -> Vec<(String, Vec<PathBuf>)> {
+    pub fn case_sensitive_exact_name(value: &str, search_path: &str) -> Vec<DirContent> {
         let dir_tree_cache = &DIR_TREE_CACHE.lock().unwrap();
         let result = dir_tree_cache.get(value).cloned().unwrap_or_default();
 
-        vec![(value.to_string(), result)]
+        to_dir_content(&value.to_string(), &result, search_path)
     }
 
-    pub fn not_case_sensitive_not_exact_name(lower_value: &str) -> Vec<(String, Vec<PathBuf>)> {
+    pub fn not_case_sensitive_not_exact_name(lower_value: &str, search_path: &str) -> Vec<DirContent> {
         let dir_tree_cache = &DIR_TREE_CACHE.lock().unwrap();
 
         dir_tree_cache
             .par_iter()
             .filter(|(key, _)| key.to_lowercase().contains(lower_value))
-            .map(|(key, paths)| (key.clone(), paths.to_owned()))
-            .collect::<Vec<(String, Vec<PathBuf>)>>()
+            .flat_map(|(name, paths)| to_dir_content(name, paths, search_path))
+            .collect()
     }
 
-    pub fn case_sensitive_not_exact_name(value: &str) -> Vec<(String, Vec<PathBuf>)> {
+    pub fn case_sensitive_not_exact_name(value: &str, search_path: &str) -> Vec<DirContent> {
         let dir_tree_cache = &DIR_TREE_CACHE.lock().unwrap();
 
         dir_tree_cache
             .par_iter()
             .filter(|(key, _)| key.contains(value))
-            .map(|(key, paths)| (key.clone(), paths.to_owned()))
-            .collect::<Vec<(String, Vec<PathBuf>)>>()
+            .flat_map(|(name, paths)| to_dir_content(name, paths, search_path))
+            .collect()
     }
+}
+
+fn to_dir_content(name: &String, paths: &Vec<PathBuf>, path: &str) -> Vec<DirContent> {
+    paths
+        .iter()
+        .filter_map(|file_folder_path| {
+            if !file_folder_path.starts_with(path) {
+                return None;
+            }
+
+            let file_path = file_folder_path.join(&name);
+            let file_path_string = file_path.to_str().unwrap().to_string();
+
+            match fs::metadata(file_path) {
+                Ok(metadata) => Some(DirContent::from_metadata(
+                    file_path_string,
+                    name.clone(),
+                    metadata,
+                )),
+                Err(_) => None,
+            }
+        })
+        .collect()
 }
 
 pub fn discover_disk(disk_path: &String) -> Result<u128, String> {
--- src-tauri/src/fs_app/get_dir_content.rs
+++ src-tauri/src/fs_app/get_dir_content.rs
@@ -2,7 +2,6 @@
 use super::cacher::discover::DirTreeSearcher;
 use super::dir_content::DirContent;
 use super::error_format::stringify;
-use rayon::prelude::*;
 use std::fs;
 
 #[tauri::command]
@@ -26,46 +25,21 @@ pub fn get_dir_content(path: &str) -> Result<Vec<DirContent>, String> {
             .collect::<Vec<DirContent>>()
     )
 }
-
+ 
 
 #[tauri::command]
 pub async fn search_by_name(path: &str, file_name: &str, only_absolute: bool, case_sensitive: bool) -> Result<Vec<DirContent>, ()> {
     let result = if only_absolute {
         match case_sensitive {
-            true => DirTreeSearcher::case_sensitive_exact_name(file_name),
-            false => DirTreeSearcher::not_case_sensitive_exact_name(&file_name.to_ascii_lowercase())
+            true => DirTreeSearcher::case_sensitive_exact_name(file_name, path),
+            false => DirTreeSearcher::not_case_sensitive_exact_name(&file_name.to_ascii_lowercase(), path)
         }
     } else {
         match case_sensitive {
-            true => DirTreeSearcher::case_sensitive_not_exact_name(file_name),
-            false => DirTreeSearcher::not_case_sensitive_not_exact_name(&file_name.to_ascii_lowercase())
+            true => DirTreeSearcher::case_sensitive_not_exact_name(file_name, path),
+            false => DirTreeSearcher::not_case_sensitive_not_exact_name(&file_name.to_ascii_lowercase(), path)
         }
     };
 
-    Ok(result
-        .into_par_iter()
-        .flat_map(|(name, paths)| {
-            paths
-                .into_iter()
-                .filter_map(|file_folder_path| {
-                    if !file_folder_path.starts_with(path) {
-                        return None;
-                    }
-    
-                    let file_path = file_folder_path.join(&name);
-                    let file_path_string = file_path.to_str().unwrap().to_string();
-        
-                    match fs::metadata(file_path) {
-                        Ok(metadata) => Some(DirContent::from_metadata(
-                            file_path_string,
-                            name.clone(),
-                            metadata,
-                        )),
-                        Err(_) => None,
-                    }
-                })
-                .collect::<Vec<DirContent>>()
-        })
-        .collect::<Vec<DirContent>>()
-    )
+    Ok(result)
 }
